14e6cb211c8f0dbbb7ac0972028e0c0aeaef2fb6
Bhanu Tejaswini Kaluva <bhanutejaswinikaluva@gmail.com>
2023-11-14 14:31:43 +0530

    Assignment-3 completed All Tasks and Completed HTTPS Exercise

--- routes/users.js
+++ routes/users.js
@@ -8,8 +8,13 @@ var router = express.Router();
 router.use(bodyParser.json());
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', authenticate.verifyOrdinaryUser, authenticate.verifyAdmin, function(req, res, next) {
+  User.find({}, function (err, user) {
+    if (err) throw err;
+    res.json(user);
+});
+
+  // res.send('respond with a resource');
 });
 
 router.post('/signup', (req, res, next) => {
@@ -43,24 +48,52 @@ router.post('/signup', (req, res, next) => {
 });
 
 router.post('/login', passport.authenticate('local'), (req, res) => {
-  
-  var token = authenticate.getToken({_id: req.user._id});
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'application/json');
-  res.json({success: true, token: token, status: 'You are successfully logged in!'});
+  if (err) {
+    return next(err);
+  }
+  if (!user) {
+    return res.status(401).json({
+      err: info
+    });
+  }
+  req.logIn(user, function(err) {
+    if (err) {
+      return res.status(500).json({
+        err: 'Could not log in user'
+      });
+    }
+
+    var token = Verify.getToken(user);
+            res.status(200).json({
+      status: 'Login successful!',
+      success: true,
+      token: token
+    });
+  })(req,res,next);
 });
+  
+  // var token = authenticate.getToken({_id: req.user._id});
+  // res.statusCode = 200;
+  // res.setHeader('Content-Type', 'application/json');
+  // res.json({success: true, token: token, status: 'You are successfully logged in!'});
+// });
 
 router.get('/logout', (req, res) => {
-  if (req.session) {
-    req.session.destroy();
-    res.clearCookie('session-id');
-    res.redirect('/');
-  }
-  else {
-    var err = new Error('You are not logged in!');
-    err.status = 403;
-    next(err);
-  }
+  req.logout();
+  res.status(200).json({
+    status: 'Bye!'
+  });
+
+  // if (req.session) {
+  //   req.session.destroy();
+  //   res.clearCookie('session-id');
+  //   res.redirect('/');
+  // }
+  // else {
+  //   var err = new Error('You are not logged in!');
+  //   err.status = 403;
+  //   next(err);
+  // }
 });
 
 module.exports = router;
